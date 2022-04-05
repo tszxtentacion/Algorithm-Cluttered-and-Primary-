1540620d2976aefd33314945e2453e0b197eafd6
tszxtentacion <77733785+tszxtentacion@users.noreply.github.com>
2022-04-05 20:15:30 +0800

    bfs版本

--- 13_方格游玩.cpp
+++ 13_方格游玩.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <algorithm>
 #include <queue>
+#include <cstring>
+#define a first
+#define b second
 using namespace std;
 const int N = 304;
 typedef pair<int, int> PII;
@@ -8,30 +11,39 @@ typedef long long ll;
 //#define x first
 //#define y second
 int ans = 1e9, n, m;
-int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
+int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1}, dist[N][N];
 char g[N][N];
 bool st[N][N];
-void dfs(int a, int b, int cnt)
+int bfs()
 {
-    if (a == n - 1 && b == m - 1)
+    queue<PII> q;
+    PII start;
+    start.a = 0, start.b = 0;
+    q.push(start);
+    st[0][0] = true;
+    while (q.size())
     {
-        ans = min(ans, cnt);
-        return;
+        PII t = q.front();
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int x = t.a + dx[i], y = t.b + dy[i];
+            if (st[x][y] || x < 0 || x >= n || y < 0 || y >= m)
+                continue;
+            if (g[x][y] == '2' && g[t.a][t.b] == '2')
+                continue;
+            if (g[x][y] == '2')
+                dist[x][y] = dist[t.a][t.b] + 1;
+            else
+                dist[x][y] = dist[t.a][t.b];
+            st[x][y] = true;
+            cout << t.a << ',' << t.b << "-->" << x << "," << y << ": " << dist[t.a][t.b] << endl;
+            q.push(make_pair(x, y));
+            if (x == n - 1 && y == m - 1)
+                return dist[x][y];
+        }
     }
-    st[a][b] = true;
-    for (int i = 0; i < 4; i++)
-    {
-        int x = a + dx[i], y = b + dy[i];
-        if (st[x][y] || x < 0 || x >= n || y < 0 || y >= m)
-            continue;
-        if (g[x][y] == '2' && g[a][b] == '2')
-            continue;
-        if (g[x][y] == '2')
-            dfs(x, y, cnt + 1);
-        else
-            dfs(x, y, cnt);
-    }
-    st[a][b] = false;
+    return -1;
 }
 
 int main()
@@ -39,7 +51,7 @@ int main()
     cin >> n >> m;
     for (int i = 0; i < n; i++)
         cin >> g[i];
-    dfs(0, 0, 0);
-    cout << ans << endl;
+    memset(dist, 0, sizeof dist);
+    cout << bfs() << endl;
     return 0;
 }
